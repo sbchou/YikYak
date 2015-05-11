2e8c963e044526dba4e9eb656dad69dc7338a638
Cat <cat@Cats-MacBook-Air.local>
2015-05-11 14:09:34 -0400

    add some scripts

---
+++ scripts/untar.sh
@@ -0,0 +1,33 @@
+
+cd ../tagged_unpacked/California\ Institute\ of\ Technology && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Carnegie\ Mellon\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Cedar\ Crest\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Columbia\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Cornell\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Dartmouth\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Georgia\ Institute\ of\ Technology && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Harvard\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Lehigh\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/MIT && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Mills\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Mount\ Holyoke\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Penn\ State\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Purdue\ University\ West\ Lafayette && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Simmons\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Smith\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Spelman\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Stanford && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Sweet\ Briar\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Syracuse\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/UC\ Berkeley && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Univeristy\ of\ Texas\ Austin && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ California\ Santa\ Barbara && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ Florida && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ Illinois\ Urbana\ Champaign && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ Iowa && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ Michigan\ Ann\ Arbor && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ Pennsylvania && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/University\ of\ Wisconsin\ Madison && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Wellesley\ College && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/West\ Virginia\ University && gunzip *.gz && echo 'done'& \
+cd ../tagged_unpacked/Yale\ University && gunzip *.gz && echo 'done'& \
