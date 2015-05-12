1940241ec3000f852e78807f1597f9e3c5af96bb
Cat <cat@Cats-MacBook-Air.local>
2015-05-12 16:38:11 -0400

    add decode utf8

--- scripts/process_json.py
+++ scripts/process_json.py
@@ -7,16 +7,10 @@ import re
 import time
 import csv
 
-"""
-def json_to_csv(outpath="/Users/cat/Dropbox (MIT)/LSM/YikYak/data/csv"):
-    with open(outpath, 'wb') as csvfile:
-    
-    with open('eggs.csv', 'wb') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=' ',
-                                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-                    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-"""
+# Twitter username:
+USERNAME_RE = re.compile(r"""(?:@[\w_]+)""", re.VERBOSE | re.I | re.UNICODE)
+URL_RE = re.compile(r'https?:\/\/.*[\r\n]*', re.VERBOSE | re.I | re.UNICODE)
+ 
 
 def preproc_name(schoolname):
     return schoolname.strip().replace(" ", "_").lower()
@@ -37,25 +31,29 @@ def get_twokens(filename):
     with open(filename) as fi:
         for line in fi:
             t = json.loads(line)    
-            text = t['body'].encode('utf8')
+            text = t['body'].decode('utf8')
             tokens = twokenize.tokenizeRawTweetText(text)
             tweets.append(tokens)
     return tweets
 
-
+### CALL this
 def get_words(filename):
     """For topic model, just find words > 3 letters"""
     tweets = []
     with open(filename) as fi:
         for line in fi:
-            t = json.loads(line)    
-            text = t['body'].encode('utf8')
-            text = text.strip().lower()\
-                    .replace("http", "")\
-                    .replace("https", "")
-            words = re.findall(' (\w{3,})', text)
-            if words:
-                yield words
+            t = json.loads(line)   
+            lang = t["twitter_lang"]
+            if lang == "en":
+                try:
+                    text = t['body'].decode('utf8').lower()
+                    text = URL_RE.sub(" ", text)
+                    text = USERNAME_RE.sub(" ", text)
+                    words = re.findall(' (\w{3,})', text)
+                    if words:
+                        yield words
+                except:
+                    continue
 
 def get_school_words(path):
     files = glob.glob(path + "/*.json") 
@@ -71,11 +69,11 @@ def get_tokens_but_cu():
     schools = glob.glob(SCHOOLS_PATH + "/*")
     rest = schools[:3] + schools[4:]
     cu = schools[3]
-    cu_words = process_json.get_school_words(cu)
+    cu_words = get_school_words(cu)
     rest_words = []
     for school in rest:
         print school
-        rest_words += process_json.get_school_words(school)
+        rest_words += get_school_words(school)
 
     return cu_words, rest_words
 
