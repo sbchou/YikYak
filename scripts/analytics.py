5d308c8d78abaea3bd5ac0a0a7202f34aecdcb85
Cat <cat@Cats-MacBook-Air.local>
2015-05-11 22:50:44 -0400

    add cts by school

--- scripts/analytics.py
+++ scripts/analytics.py
@@ -1,4 +1,7 @@
 import glob
+import re
+
+URL_PATT = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
 def get_lengths(dir_path):
     """return a giant vector of char length of ea. tweet"""
@@ -10,3 +13,18 @@ def get_lengths(dir_path):
                 lengths.append(len(line.strip()))
     return lengths
 
+def gen_tweet_list(dir_path):
+    files = glob.glob(dir_path + "/*")
+    vect = []
+    for school in files:
+        with open(school) as f:
+            for line in f:
+                vect.append(line.strip())
+    return vect
+
+
+def contains_url(tweet):
+    if re.findall(URL_PATT, tweet):
+        return 1
+    else:
+        return 0
