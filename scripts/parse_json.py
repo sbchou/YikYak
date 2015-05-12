18f979bf4ea2c4cf1fe547cc5420ad8d12681a4a
Cat <cat@Cats-MacBook-Air.local>
2015-05-11 21:30:44 -0400

    add lengths vector

--- scripts/parse_json.py
+++ scripts/parse_json.py
@@ -1,23 +1,15 @@
 import json
 import glob
-import preprocess_tweets
+import twokenize
 import os
 import cPickle as pickle
+import re
 
 def preproc_name(schoolname):
     return schoolname.strip().replace(" ", "_").lower()
 
-def get_tweets(filename):
-    tweets = []
-    with open(filename) as fi:
-        for line in fi:
-            t = json.loads(line)    
-            text = t['body'].encode('utf8')
-            text = preprocess_tweets.preprocess(text)
-            tweets.append(text)
-    return tweets
-
 def get_text(filename):
+    """ Just the pure body of tweets."""
     tweets = []
     with open(filename) as fi:
         for line in fi:
@@ -26,42 +18,70 @@ def get_text(filename):
             tweets.append(text)
     return tweets
 
-
-def get_tokens(filename):
+def get_twokens(filename):
+    """CMU Twokenizer."""
     tweets = []
     with open(filename) as fi:
         for line in fi:
             t = json.loads(line)    
             text = t['body'].encode('utf8')
-            text = preprocess_tweets.preprocess(text)
-            tweets.append(text)
+            tokens = twokenize.tokenizeRawTweetText(text)
+            tweets.append(tokens)
     return tweets
 
 
-def parse_text(dir_path):
+def get_words(filename):
+    """For topic model, just find words > 3 letters"""
+    tweets = []
+    with open(filename) as fi:
+        for line in fi:
+            t = json.loads(line)    
+            text = t['body'].encode('utf8')
+            words = re.findall(' (\w{3,} )', text)
+            words = " ".join(words)\
+                        .strip()\
+                        .lower()\
+                        .replace("http", "")\
+                        .replace("https", "")
+            if words:
+                yield words
+
+def parse_tweets_for_topics(dir_path):
+    """ FOR TOPIC MODELS, JUST WORDS """
     schoolname = os.path.basename(dir_path)
     schoolname = preproc_name(schoolname)
     print schoolname
     files = glob.glob(dir_path + "/*.json")
-    out = "/Users/cat/Dropbox (MIT)/LSM/YikYak/data/tokens/" + \
+    out = "/Users/cat/Dropbox (MIT)/LSM/YikYak/data/words/" + \
           schoolname + ".tsv"
-    print out
-    out_pickle = "/Users/cat/Dropbox (MIT)/LSM/YikYak/data/pickles/" + \
+    out_pickle = "/Users/cat/Dropbox (MIT)/LSM/YikYak/data/pickled_words/" + \
           schoolname + ".pickle"  
  
     biglist = []
 
     with open(out, 'w') as outfile:
         for f in files:
-            tweets = get_tweets(f)
-            tsv = ["\t".join(t) for t in tweets]
-            outfile.write("\n".join(tsv))
-            biglist.append(tweets)
+            tweets = [t for t in get_words(f)]
+            #import pdb; pdb.set_trace()
+            outfile.write("\n".join(tweets))
+            biglist += tweets
 
     pickle.dump(biglist, open(out_pickle, 'wb'))
 
 
+def parse_tweets(dir_path):
+    """ Just pure encoded tweets. Dump to school csvs"""
+    schoolname = os.path.basename(dir_path)
+    schoolname = preproc_name(schoolname)
+    print schoolname
+    files = glob.glob(dir_path + "/*.json")
+    out = "/Users/cat/Dropbox (MIT)/LSM/YikYak/data/tokens/" + \
+          schoolname + ".tsv"
 
-
+    with open(out, 'w') as outfile:
+        for f in files:
+            tweets = get_tokens(f)
+            tsv = ["\t".join(t) for t in tweets]
+            outfile.write("\n".join(tsv))
 
 
